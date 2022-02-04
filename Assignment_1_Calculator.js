a1c895fb26372159d7c6b2f6c8b404ea5eab8939
anjali954 <mehendirattaanjali@gmail.com>
2022-02-04 16:43:49 +0530

    assignments

--- Assignment_1_Calculator.js
+++ Assignment_1_Calculator.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+var a =10;
+var b=20;
+var sum =a+b;
+console.log(sum);
+// Ans: 30
+
+var a =10;
+var b=20;
+var subs =a-b;
+console.log(subs);
+ // Ans: -10
+
+
+var a =10;
+var b=20;
+var multiply =a*b;
+console.log(multiply);
+// Ans: 200
+
+var a =10;
+var b=20;
+var divide =a/b;
+console.log(divide);
+// Ans:  0.5
+
+
