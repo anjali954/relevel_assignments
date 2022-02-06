f70b8be575e3082358182355fa053b9191a305d2
anjali954 <mehendirattaanjali@gmail.com>
2022-02-06 18:58:39 +0530

    Assignment2

--- even_odd_assignment.js
+++ even_odd_assignment.js
@@ -5,3 +5,19 @@ console.log("a is even");
 else{
 console.log("a is odd");
 }
+
+// Program to check if a number is a perfect square 
+const num = 16;
+const perfectSquare = num => {
+   let a = 1;
+   while(a * a <= num){
+      if(a * a !== num){
+         a++;
+         continue;
+      };
+      return true;
+   };
+   return false;
+};
+console.log(perfectSquare(num));
+
